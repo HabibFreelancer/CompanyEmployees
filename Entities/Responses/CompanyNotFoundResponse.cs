1bd4a35a768f9eec85a98eb1c371abd26c02907f
Habib BEN RADHOUENE <hben-radhouene@domusvi.com>
2024-05-04 08:54:01 +0200

    move library class and add ApiResponse Abstract

---
+++ Entities/Responses/CompanyNotFoundResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Responses
+{
+
+    /*The class inherits from the ApiNotFoundResponse abstract class, which
+again inherits from the ApiBaseResponse class. It accepts an id
+parameter and creates a message that sends to the base class*/
+    public sealed class CompanyNotFoundResponse : ApiNotFoundResponse
+    {
+        public CompanyNotFoundResponse(Guid id)
+        : base($"Company with id: {id} is not found in db.")
+        { }
+    }
+}
