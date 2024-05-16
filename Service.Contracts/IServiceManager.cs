604b4b98d7d293aea6e9c4395999f1efe5ac10e4
Habib BEN RADHOUENE <hben-radhouene@domusvi.com>
2024-05-16 11:19:19 +0200

    delete company service

--- Service.Contracts/IServiceManager.cs
+++ Service.Contracts/IServiceManager.cs
@@ -8,7 +8,6 @@ namespace Service.Contracts
 {
     public interface IServiceManager
     {
-        ICompanyService CompanyService { get; }
         IEmployeeService EmployeeService { get; }
 
         IAuthenticationService AuthenticationService { get; }
--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -17,7 +17,6 @@ namespace Service
 {
     public sealed class ServiceManager : IServiceManager
     {
-        private readonly Lazy<ICompanyService> _companyService;
         private readonly Lazy<IEmployeeService> _employeeService;
         private readonly Lazy<IAuthenticationService> _authenticationService;
 
@@ -26,15 +25,14 @@ namespace Service
             UserManager<User> userManager,
         IOptions<JwtConfiguration> configuration)
         {
-            _companyService = new Lazy<ICompanyService>(() => new
-            CompanyService(repositoryManager, logger, mapper));
+          
             _employeeService = new Lazy<IEmployeeService>(() => new
             EmployeeService(repositoryManager, logger, mapper, dataShaper));
             _authenticationService = new Lazy<IAuthenticationService>(() =>
                             new AuthenticationService(logger, mapper, userManager, configuration));
 
         }
-        public ICompanyService CompanyService => _companyService.Value;
+      
         public IEmployeeService EmployeeService => _employeeService.Value;
         public IAuthenticationService AuthenticationService => _authenticationService.Value;
 
